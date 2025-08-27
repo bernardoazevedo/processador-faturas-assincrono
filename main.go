40eaa1f78049c6533b9c58f522e44859261957f6
Bernardo Azevedo <bernardoazevedo.8@gmail.com>
2025-08-27 08:46:46 -0300

    iniciando serviço de enviar notificações ao iniciar aplicação

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 		log.Fatal("Error connecting to rabbitmq")
 	}
 
-	// go message.EnviaNotificacoes()
+	go message.EnviaNotificacoes()
 
 	router := gin.Default()
 	loadRoutes(router)
