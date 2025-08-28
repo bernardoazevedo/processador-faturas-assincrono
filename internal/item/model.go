e28a199a311c1e5098d6144b0e0d34986f91c822
Bernardo Azevedo <bernardoazevedo.8@gmail.com>
2025-08-28 13:35:28 -0300

    removendo arquivos não usados

--- internal/item/model.go
+++
@@ -1,7 +0,0 @@
-package item
-
-type Item struct {
-	Id 				string 	`json:"id"`
-	Descricao 		string	`json:"descricao"`
-	PrecoCentavos 	int 	`json:"preco_centavos"`
-}
--- internal/pessoa/model.go
+++
@@ -1,7 +0,0 @@
-package pessoa
-
-type Pessoa struct {
-	Id 	 string `json:"id"`
-	Nome string `json:"nome"`
-	Cnpj string `json:"cnpj"`
-}
